811cc0537cc42fd1c6a92a5d923ee79b389e1e8c
Green0x0y <66325429+Green0x0y@users.noreply.github.com>
2023-05-30 16:06:01 +0200

    frontend

--- motorcyclesFront/motorcycleapp/src/app/app.component.ts
+++ motorcyclesFront/motorcycleapp/src/app/app.component.ts
@@ -27,4 +27,27 @@ export class AppComponent implements OnInit {
       }
     )
   }
+
+  public onOpenModal(motorcycle: Motorcycle | null, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button. style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode == 'add'){
+      button.setAttribute('data-target', '#addMotorcycleModal')
+    }
+    if (mode == 'edit'){
+      button.setAttribute('data-target', '#editMotorcycleModal')
+    }
+    if (mode == 'delete'){
+      button.setAttribute('data-target', '#deleteMotorcycleModal')
+    }  
+    if (container !== null) {
+      container.appendChild(button);
+    }
+    button.click();
+  }
+  
+
 }
